954d2ac36e3209d35e5baa41c3383afed57ae131
clumsy261 <rucareanueric@gmail.com>
2026-08-11 12:26:53 +0300

    test alert function

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ let greetMsgEl;
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  alert(await invoke("alert"));
 }
 
 window.addEventListener("DOMContentLoaded", () => {
